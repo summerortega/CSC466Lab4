1157612707c9af7bff7ffc5874c6fae7b6183895
Diego Melgoza <drmelgoza@icloud.com>
2026-05-13 16:27:21 -0700

    mk II complete

--- hclustering.py
+++ hclustering.py
@@ -7,7 +7,7 @@ def main(filename, threshold):
     df = read_csv(filename)
     data_points = [[x] for x in df.to_numpy()]
     clusters = [{"type": "leaf", "data": x} for x in df.to_numpy()]
-    matrix = pairwise_distances(data_points, Y=None, metric='euclidean')
+    matrix = pd.DataFrame(pairwise_distances(data_points, Y=None, metric='euclidean'))
 
     while len(clusters) > 1:
         # for j in range(len(clusters)):
@@ -16,28 +16,22 @@ def main(filename, threshold):
 
         # calulcating minium distanc eand signle loink distance
         matrix[matrix == 0] = np.inf
-        a = np.argmin(matrix) % len(matrix)
-        b = np.argmin(matrix) // len(matrix)
+
+        a = matrix.min(axis=0).idxmin()
+        b = matrix.iloc[a].idxmin()
 
         matrix[matrix == np.inf] = 0
-        df_matrix = pd.DataFrame(matrix)
 
-        a = df_matrix.min(axis=0).idxmin()
-        b = df_matrix.iloc[a].idxmin()
-
-        row_a = df_matrix[a]
-        row_a[row_a == np.inf] = 0
-
-        row_b = df_matrix[b]
-        row_b[row_b == np.inf] = 0
+        row_a = matrix[a]
+        row_b = matrix[b]
 
         min_arr = np.minimum(row_a, row_b)
 
-        df_matrix = df_matrix.iloc[a] = min_arr
-        df_matrix.iloc[:, a] = min_arr
+        matrix.iloc[a] = min_arr
+        matrix.iloc[:, a] = min_arr
 
-        df_matrix = df_matrix.drop(b, axis=0)
-        df_matrix = df_matrix.drop(b, axis=1)
+        matrix = matrix.drop(b, axis=0)
+        matrix = matrix.drop(b, axis=1)
 
         data_points[a] = data_points[a].extend(data_points[b])
         data_points.pop(b)

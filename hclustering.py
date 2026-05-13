00d27ee41d2d945cf3402a9741cf34cce040d2d3
summerortega <summermortega@gmail.com>
2026-05-13 15:40:04 -0700

    implemented distance matrix calculations/singlelink

---
+++ hclustering.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import numpy
+from sklearn.metrics import pairwise_distances
+
+
+from kmeans import read_csv
+def main(filename, threshold):
+    df = read_csv(filename)
+    clusters = [x for x in df.to_numpy()]
+
+    while len(clusters) > 1:
+        matrix = pairwise_distances(clusters, Y=None, metric='euclidean')
+        # for j in range(len(clusters)):
+        #     for k in range(j+1, len(clusters)):
+        #         matrix[j][k] = distance_calculation(clusters[j], clusters[k])
+
+        # calulcating minium distanc eand signle loink distance
+        matrix[matrix == 0] = np.inf
+        a = np.argmin(matrix) % len(matrix)
+        b = np.argmin(matrix) // len(matrix)
+
+        matrix[matrix == np.inf] = 0
+        df_matrix = pd.DataFrame(matrix)
+
+        a = df_matrix.min(axis=0).idxmin()
+        b = df_matrix.iloc[a].idxmin()
+
+        row_a = df_matrix[a]
+        row_a[row_a == np.inf] = 0
+
+        row_b = df_matrix[b]
+        row_b[row_b == np.inf] = 0
+
+        min_arr = np.minimum(row_a, row_b)
+
+        df_matrix = df_matrix.iloc[a] = min_arr
+        df_matrix.iloc[:, a] = min_arr
+
+        df_matrix = df_matrix.drop(b, axis=0)
+        df_matrix = df_matrix.drop(b, axis=1)
+
+        clusters = []
+
+
+
+
+
+
+
+
+
+

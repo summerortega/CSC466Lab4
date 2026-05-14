6da4e2ab87d7f04b2d121e902e7438956dbbe99b
Diego Melgoza <drmelgoza@icloud.com>
2026-05-13 17:24:09 -0700

    add dentrogram

--- hclustering.py
+++ hclustering.py
@@ -5,20 +5,16 @@ from sklearn.metrics import pairwise_distances
 from kmeans import read_csv
 def main(filename, threshold):
     df = read_csv(filename)
-    data_points = [[x] for x in df.to_numpy()]
-    clusters = [{"type": "leaf", "data": x} for x in df.to_numpy()]
+    arr = df.to_numpy()
+    data_points = [x for x in arr]
+    clusters = [{"type": "leaf", "data": x} for x in arr]
     matrix = pd.DataFrame(pairwise_distances(data_points, Y=None, metric='euclidean'))
 
-    while len(clusters) > 1:
-        # for j in range(len(clusters)):
-        #     for k in range(j+1, len(clusters)):
-        #         matrix[j][k] = distance_calculation(clusters[j], clusters[k])
-
-        # calulcating minium distanc eand signle loink distance
+    while len(matrix) > 1:
         matrix[matrix == 0] = np.inf
 
         a = matrix.min(axis=0).idxmin()
-        b = matrix.iloc[a].idxmin()
+        b = matrix.loc[a].idxmin()
 
         matrix[matrix == np.inf] = 0
 
@@ -27,17 +23,20 @@ def main(filename, threshold):
 
         min_arr = np.minimum(row_a, row_b)
 
-        matrix.iloc[a] = min_arr
-        matrix.iloc[:, a] = min_arr
+        matrix.loc[b] = min_arr
+        matrix.loc[:, b] = min_arr
 
-        matrix = matrix.drop(b, axis=0)
+        matrix = matrix.drop(b, axis=0).reset_index(drop=True)
         matrix = matrix.drop(b, axis=1)
-
-        data_points[a] = data_points[a].extend(data_points[b])
-        data_points.pop(b)
+        matrix.columns = range(matrix.columns.size)
 
         clusters[a] = {"type": "node", "height": 21, "nodes": [clusters[a], clusters[b]]}
+        print(clusters[b])
+
         clusters.pop(b)
+
+    dentrogram = {"type": "root", "height": 21, "nodes": clusters[0]}
+
 
 
 

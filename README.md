fcf7f89ed700f2912e865d8d1a19b4042a839ba6
summerortega <summermortega@gmail.com>
2026-05-18 00:53:01 -0700

    updated json output for hclustering. added evaluation report

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ sorteg16@calpoly.edu
 
 Diego Melgoza
 drmelgoz@calpoly.edu
+
+
+
+The optional third argument is the zero-based column index of the ground-truth class label, if one exists according to the dataset header file.
--- kmeans.py
+++ kmeans.py
@@ -1,23 +1,11 @@
 import sys
-import pandas as pd
+from evaluation import read_csv
+
 
 def main(csv_file, k):
-    df = pd.read_csv(csv_file)
+    df = read_csv(csv_file)
 
 
-def read_csv(csv_path:str) -> tuple[pd.DataFrame, pd.Series]:
-    # read entire csv
-    df = pd.read_csv(csv_path, header=None)
-
-    restrictions = df.iloc[0].astype(int)
-    # drop metadata
-    df = df.drop(index=0).reset_index(drop=True)
-
-    clean_df = restrictions[restrictions == 1].index
-
-    x = df.loc[:, clean_df].astype(float)
-
-    return x
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
